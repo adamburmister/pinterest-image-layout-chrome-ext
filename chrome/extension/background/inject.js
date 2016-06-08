726f6153784daea8809dc0b0e78e2fbdf0c8fe6e
Adam Burmister <adam.burmister@pinterest.com>
2016-06-08 14:20:39 -0700

    Getting to success on inject

--- chrome/extension/background/inject.js
+++ chrome/extension/background/inject.js
@@ -17,23 +17,23 @@ function loadScript(name, tabId, cb) {
     .then(fetchRes => {
       // Load redux-devtools-extension inject bundle,
       // because inject script and page is in a different context
-      const request = new XMLHttpRequest();
-      request.open('GET', 'chrome-extension://lmhkpmbekcpmknklioeibfkpmmfibljd/js/inject.bundle.js');  // sync
-      request.send();
-      request.onload = () => {
-        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-          chrome.tabs.executeScript(tabId, { code: request.responseText, runAt: 'document_start' });
-        }
-      };
+      // const request = new XMLHttpRequest();
+      // request.open('GET', 'chrome-extension://lmhkpmbekcpmknklioeibfkpmmfibljd/js/inject.bundle.js');  // sync
+      // request.send();
+      // request.onload = () => {
+      //   if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+      //     chrome.tabs.executeScript(tabId, { code: request.responseText, runAt: 'document_start' });
+      //   }
+      // };
       chrome.tabs.executeScript(tabId, { code: fetchRes, runAt: 'document_end' }, cb);
     });
   }
 }
 
-const arrowURLs = ['^https://github\\.com'];
+// const arrowURLs = ['^https://github\\.com'];
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (changeInfo.status !== 'loading' || !tab.url.match(arrowURLs.join('|'))) return;
+  if (changeInfo.status !== 'loading'/* || !tab.url.match(arrowURLs.join('|'))*/) return;
 
   const result = await isInjected(tabId);
   if (chrome.runtime.lastError || result[0]) return;
